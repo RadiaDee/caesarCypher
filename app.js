0ed12a11a3addf0c64d66380eff99f0f405a9848
Radia Dakhama <radiadakhama@gmail.com>
2022-04-13 16:21:02 -0600

    original version

---
+++ app.js
@@ -0,0 +1,97 @@
+/*
+
+
+A Caesar Cypher is a classic encryption 
+technique favored by the ancient Romans.
+
+Define a function, `caesarCypher`, that accepts two arguments:
+  1. secret (string) - the string we want to encrypt
+  2. shift (num) - the number we want to rotate each 
+  letter in the string by
+
+`caesarCypher` should return an encrypted version of 
+the string shifted by the number of letters supplied. For 
+example, the string 'abc' shifted by 2 should return 'ced'. 
+Spaces between words should not be rotated. Note that any 
+shifts that exceed the end of the alphabet should wrap 
+around. For example, the string 'xyz' shifted by 2 should 
+return 'zab', the string 'hello' shifted by 13 should 
+return 'urryb'.
+
+You can assume the shift number will be a positive integer. 
+Additionally, you may assume that the secret string will 
+be entirely lower case
+
+
+
+*/
+// original version
+// convert letter to alphabet position number
+function findLetterPosition(letter, array){  
+    for(let i=0; i<array.length; i++){  // iterate through lettersArray
+      if (letter === array[i]){ // match letter to alphabet letter
+        return i;   // returns letter position in alphabet
+      }
+    }
+};
+  
+function caesarCypher(string, num){
+    const lettersArray = ["a", "b", "c", "d", "e", "f", "g", 
+    "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", 
+    "s", "t", "u", "v", "w", "x", "y", "z"];
+    let encryptedString ="";  
+    
+    for (let i=0; i<string.length; i++){ // go through the string
+        let currentLetter = string[i];  // store letter in currentLetter
+        
+        if (currentLetter === " "){
+            encryptedString = encryptedString + " ";
+            continue;
+        }
+        
+        // find position of the letter in the alphabet
+        let letterPos = findLetterPosition(currentLetter, lettersArray);  // convert the first letter of the word to alphabet number
+        let encryptedLetterPos = (letterPos +num)%26; // the letter index + shift aka the encrypted letter
+        let encryptedLetter = lettersArray[encryptedLetterPos];  // convert encrypted index to encrypted character
+        encryptedString = encryptedString + encryptedLetter;  // concatenate encrypted letter to encryptedstring                         
+    }
+    
+    console.log("the encrypted word is "+ encryptedString);
+    return encryptedString;
+};
+
+
+/*
+describe('Caesar Cypher', () => {
+
+  it('is a function?', () => {
+    expect(typeof caesarCypher).toEqual('function');
+  });
+
+  it('rotates a letter by the number passed in', () => {
+    let returnedValue = caesarCypher('a', 2);
+    expect(returnedValue).toEqual('c');
+  });
+
+  it('rotates every letter in the string by the supplied letter', () => {
+    let returnedValue = caesarCypher('doggo', 7);
+    expect(returnedValue).toEqual('kvnnv');
+  });
+
+  it('wraps around to the beginning of the alphabet when necessary', () => {
+    let returnedValue = caesarCypher('hello', 13);
+    expect(returnedValue).toEqual('uryyb');
+  });
+
+  it('retains spaces between encrypter world', () => {
+    let returnedValue = caesarCypher('hello world', 13);
+    expect(returnedValue).toEqual('uryyb jbeyq');
+  });
+
+});
+
+
+
+
+*/
+
